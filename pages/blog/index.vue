a2fafe937631aa0ca3270cef871c09103ab7a7af
Chloe <cugttweb@pm.me>
2020-11-21 22:04:52 +0000

    tweaking styling..

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,12 +5,12 @@
     </div>
     <ul class="block md:flex flex-wrap mx-auto lg:w-1/2">
         <li class="p-5 shadow-lg md:w-1/2" v-for="post in posts" :key="post.id">
-            <nuxt-link :to="{ name: 'blog-slug', params: { slug: post.slug } }">
+            <!--<nuxt-link :to="{ name: 'blog-slug', params: { slug: post.slug } }">-->
                 <p class="text-small text-green-500">{{ formatDate(post.date) }}</p>
-                <h3 class="py-2 text-white">{{ post.title }}</h3>
-                <p class="py-2 text-white">{{ post.description }}</p>
+                <h3 class="py-4 text-white">{{ post.title }}</h3>
+                <!-- <p class="py-2 text-white">{{ post.description }}</p>-->
                 <a :to="{ name: 'blog-slug', params: { slug: post.slug }}" class="bg-red-800 px-3 py-2 rounded text-white hover:bg-blue-800 ">Read More</a>
-            </nuxt-link>
+            <!--</nuxt-link>-->
         </li>
     </ul>
 </div>
