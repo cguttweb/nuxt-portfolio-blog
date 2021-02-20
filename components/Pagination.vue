49f6d687dbf0603019ebfa1bfa4784d673a166d8
Chloe <cguttweb@pm.me>
2021-02-20 16:46:04 +0000

    attempting pagination with vuejs-paginate plugin

---
+++ components/Pagination.vue
@@ -0,0 +1,31 @@
+<template>
+    <paginate class="flex justify-center space-between"
+    :page-count="5"
+    :click-handler="paginate"
+    :prev-text="'Prev'"
+    :next-text="'Next'"
+    :container-class="pagination"
+    >
+    </paginate>
+</template>
+
+<script>
+import Paginate from 'vuejs-paginate'
+
+export default {
+    components: {
+        Paginate
+    },
+    methods: {
+        paginate(pageNumber){
+            console.log(pageNumber)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.pagination {
+    @apply py-2;
+}
+</style>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -14,6 +14,7 @@
             </nuxt-link>
         </li>
     </ul>
+    <Pagination />
 </main>
 </template>
 
