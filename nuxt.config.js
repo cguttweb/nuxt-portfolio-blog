6796930338e939fc0ec367d3c35e89629b3b5de9
Chloe <cugttweb@pm.me>
2021-03-26 22:19:33 +0000

    attempting to preload fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,8 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito&family=Roboto&display=swap', preload: true }
+      { rel: 'preload', href: 'https://fonts.googleapis.com/css2?family=Nunito&family=Roboto&display=swap', as: 'style'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito&family=Roboto&display=swap' }
     ]
   },
 
