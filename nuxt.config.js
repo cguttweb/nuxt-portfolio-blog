3abfa6fd65d107196368317b634511b8e61fb1bd
Chloe <cugttweb@pm.me>
2021-04-10 21:00:42 +0100

    updated tailwind module & config

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,8 +76,12 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
+
+  tailwindcss: {
+    jit: true
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
