64d201849d8302b4e4799ad7d5e7573bb55ba03d
Chloe <cguttweb@pm.me>
2022-01-03 17:50:33 +0000

    updated blog page to reference posts subfolders

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,86 +1,90 @@
 <template>
-    <div class="bg-gray-800 text-white flex justify-center">
-        <div class="w-4/5">
-            <h1>{{ post.title }}</h1>
+  <div class="bg-gray-800 text-white flex justify-center">
+    <div class="w-4/5">
+      <h1>{{ post.title }}</h1>
 
-            <p class="pl-2 pb-2">{{ post.description }}</p>
+      <p class="pl-2 pb-2">{{ post.description }}</p>
 
-            <p class="pl-2">Post created on: {{ formatDate(post.date) }} </p>
+      <p class="pl-2">Post created on: {{ formatDate(post.date) }}</p>
 
-            <nuxt-content :document="post" />
+      <nuxt-content :document="post" />
 
-            <prev-next :prev="prev" :next="next" />
-        </div>
+      <prev-next :prev="prev" :next="next" />
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        async asyncData({ $content, params }){
-            const post = await $content('posts', params.slug).fetch()
+export default {
+  async asyncData({ $content, params }) {
+    const post = await $content("posts/", params.slug).fetch();
 
-            const [prev, next] = await $content('posts')
-            .only(['title', 'slug', 'date'])
-            .sortBy('date', 'desc')
-            .surround(params.slug)
-            .fetch()
+    const [prev, next] = await $content("posts")
+      .only(["title", "slug", "date"])
+      .sortBy("date", "desc")
+      .surround(params.slug)
+      .fetch();
 
-            return {
-                post,
-                prev,
-                next
-            }
-        },
-        head(){
-            return {
-                title: this.post.title
-            }
-        },
-        methods: {
-            formatDate(date) {
-                const dateFormatting = { year: 'numeric', month: 'numeric', day: 'numeric' }
-                return new Date(date).toLocaleDateString('en-gb', dateFormatting)
-            }
-        }
+    return {
+      post,
+      prev,
+      next
+    };
+  },
+  head() {
+    return {
+      title: this.post.title
+    };
+  },
+  methods: {
+    formatDate(date) {
+      const dateFormatting = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric"
+      };
+      return new Date(date).toLocaleDateString("en-gb", dateFormatting);
     }
+  }
+};
 </script>
 
 <style>
 h1 {
-    @apply pl-2;
-    @apply text-red-700;
-    @apply text-2xl;
+  @apply pl-2;
+  @apply text-red-700;
+  @apply text-2xl;
 }
 
 .nuxt-content h1 {
-    text-align: center;
+  text-align: center;
 }
 
 .nuxt-content h3,
 .nuxt-content h4 {
-    color: #CC0000;
-    padding: 0.5rem;
+  color: #cc0000;
+  padding: 0.5rem;
 }
 
 .nuxt-content h3 {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .nuxt-content h4 {
-    font-size: 18px;
+  font-size: 18px;
 }
 
 .nuxt-content p {
-    padding: 0.5rem;
+  padding: 0.5rem;
 }
 
 .nuxt-content li {
-    list-style: disc;
-    margin-left: 0.5rem;
-    padding: 0.5rem;
+  list-style: disc;
+  margin-left: 0.5rem;
+  padding: 0.5rem;
 }
 
 .nuxt-content a {
-    text-decoration: underline;
+  text-decoration: underline;
 }
 </style>
