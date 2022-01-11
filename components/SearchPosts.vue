b5d09b8e0ec8bea3191e964c33baa42a65fdaf56
Chloe <cguttweb@pm.me>
2022-01-11 20:16:24 +0000

    updated blog slug to reference subfolder

--- components/SearchPosts.vue
+++ components/SearchPosts.vue
@@ -1,41 +1,40 @@
 <template>
-    <div class="pl-5 py-4">
-        <label for="searchposts" class="hidden">Search Posts</label>
-        <input aria-label="Search blog posts"
-        class="bg-gray-900 border border-red-600 flex justify-center pl-2 py-1 text-white"
-        type="search" 
-        name="searchposts"
-        placeholder="Search Posts"
-        v-model="searchPosts">
-            <ul>
-                <li class="text-white" v-for="post in posts" :key="post.date">
-                    <nuxt-link :to="{ name: 'blog-slug', params: { slug: post.slug } }">
-                        {{ post.title }}
-                    </nuxt-link>
-                </li>
-            </ul>
-    </div>
+  <div class="pl-5 py-4">
+    <label for="searchposts" class="hidden">Search Posts</label>
+    <input
+      aria-label="Search blog posts"
+      class="bg-gray-900 border border-red-600 flex justify-center pl-2 py-1 text-white"
+      type="search"
+      name="searchposts"
+      placeholder="Search Posts"
+      v-model="searchPosts"
+    />
+    <ul>
+      <li class="text-white" v-for="post in posts" :key="post.date">
+        <nuxt-link :to="{ name: 'blog-slug', params: { slug: post.slug } }">
+          {{ post.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                searchPosts: '',
-                posts: []
-            }
-        },
-        watch: {
-            async searchPosts(searchPosts){
-                if(!searchPosts){
-                    this.posts = []
-                    return
-                }
-                this.posts = await this.$content('posts')
-                .limit(10)
-                .search(searchPosts)
-                .fetch()
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      searchPosts: "",
+      posts: [],
+    };
+  },
+  watch: {
+    async searchPosts(searchPosts) {
+      if (!searchPosts) {
+        this.posts = [];
+        return;
+      }
+      this.posts = await this.$content("posts/*").limit(10).search(searchPosts).fetch();
+    },
+  },
+};
 </script>
